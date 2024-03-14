6c3202e967d1afb49cc099e911981ac1c7cbed58
Roman Dronov <roman.dronov@softdev.pw>
2024-03-14 20:45:53 +0600

    feat(openapi): install openapi

--- src/random-strings/random-strings.controller.ts
+++ src/random-strings/random-strings.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get, Param, Patch } from '@nestjs/common';
 import { RandomStringsService } from './random-strings.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('random-strings')
+@ApiTags('Strings API')
 export class RandomStringsController {
   constructor(private readonly randomStringsService: RandomStringsService) {}
 
+  @ApiOperation({
+    summary: 'Find document by name',
+    description: 'Find by name',
+  })
   @Get('find-by-name/:name')
   async findByName(@Param('name') name: string) {
     return this.randomStringsService.findByName(name);
